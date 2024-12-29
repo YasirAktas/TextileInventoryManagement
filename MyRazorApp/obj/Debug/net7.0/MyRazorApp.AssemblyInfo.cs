1a62468d4ef671ed9919be77df80c76fdcb9ac1a
Ezher Memeti <ezher.memeti@gmail.com>
2024-12-29 13:38:17 +0300

    Sale Page and add sale form
    add sale functionality not working yet

--- MyRazorApp/obj/Debug/net7.0/MyRazorApp.AssemblyInfo.cs
+++ MyRazorApp/obj/Debug/net7.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f2ddf0f94c1d6be54971cb57ac644d42de41a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c0a785b45cc133396f27bf6580d269ee9c3ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
