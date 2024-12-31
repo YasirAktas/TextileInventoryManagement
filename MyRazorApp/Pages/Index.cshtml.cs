d32a601b993d1350f0cc17f7277d07ee2c8c70f2
Ezher Memeti <ezher.memeti@gmail.com>
2024-12-31 18:59:34 +0300

    Dashboard update

--- MyRazorApp/Pages/Index.cshtml.cs
+++ MyRazorApp/Pages/Index.cshtml.cs
@@ -12,7 +12,7 @@ namespace MyRazorApp.Pages
         private readonly string connectionString = "Server=127.0.0.1,1433; Database=TIMS; User ID=sa; Password=reallyStrongPwd123; Encrypt=false;";
 
         // Properties to hold the list of products and revenue
-        public List<Product> TotalProducts { get; set; }
+        public List<ProductStockSummary> TotalProducts { get; set; }
         public List<Product> LowStockProducts { get; set; }
         public decimal TotalRevenue { get; set; }
 
@@ -30,13 +30,13 @@ namespace MyRazorApp.Pages
             TotalRevenue = GetTotalRevenueFromDatabase();
         }
 
-        private List<Product> GetTotalProductsFromDatabase()
+        private List<ProductStockSummary> GetTotalProductsFromDatabase()
         {
-            var products = new List<Product>();
+            var products = new List<ProductStockSummary>();
 
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                string query = "SELECT ProductName, StockLevel FROM ProductStockView"; // Query the view
+                string query = "SELECT ProductName, TotalStockLevel, ColorVariants FROM ProductStockSummary"; // Query the view
                 SqlCommand cmd = new SqlCommand(query, con);
 
                 con.Open();
@@ -44,10 +44,11 @@ namespace MyRazorApp.Pages
                 {
                     while (reader.Read())
                     {
-                        products.Add(new Product
+                        products.Add(new ProductStockSummary
                         {
                             Name = reader.GetString(0), // ProductName
-                            StockQuantity = reader.GetInt32(1) // StockLevel
+                            StockQuantity = reader.GetInt32(1), // StockLevel
+                            ColorVariants = reader.GetInt32(2) //
                         });
                     }
                 }
@@ -74,7 +75,7 @@ namespace MyRazorApp.Pages
                         products.Add(new Product
                         {
                             Name = reader.GetString(0), // ProductName
-                            StockQuantity = reader.GetInt32(1) // StockLevel
+                            StockQuantity = reader.GetInt32(1), // StockLevel
                         });
                     }
                 }
@@ -102,5 +103,11 @@ namespace MyRazorApp.Pages
 
             return totalRevenue;
         }
+    }
+    public class ProductStockSummary
+    {
+        public string Name { get; set; }
+        public int StockQuantity { get; set; }
+        public int ColorVariants {get; set; }
     }
 }
